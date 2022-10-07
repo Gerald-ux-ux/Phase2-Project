531137096f8eb8c7e210ce4a3a561dd18051f222
gerald <you@example.com>
2022-10-07 09:20:22 +0300

    looping through the data

--- src/Fetch.js
+++ src/Fetch.js
@@ -10,4 +10,36 @@ const Fetch = () => {
       .then((response) => response.json())
       .then((data) => setData(data));
   }, [query]);
+  function handleChange(event) {
+    setQuery(event.target.value);
+  }
+  console.log(query);
 
+  return (
+    <>
+      <div>
+        {data.length === 0 ? (
+          <h3>type in a value</h3>
+        ) : (
+          data.map((name, index) => {
+            return (
+              <div className="content" key={index}>
+                <p>{name.name}</p>
+                <p>{name.url}</p>
+                <p>{name.start_time}</p>
+                <p>{name.end_time}</p>
+                <p>{name.duration}</p>
+                <p>{name.site}</p>
+                <p>{name.in_24_hours}</p>
+                <p>{name.status}</p>
+              </div>
+            );
+          })
+        )}
+      </div>
+      <Search handleChange={handleChange} />
+    </>
+  );
+};
+
+export default Fetch;
